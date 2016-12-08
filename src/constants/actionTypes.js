2186fd91cc62d2ebd7c583b53358bf4c19aa3caa
broerjuang <juangwiantoro@gmail.com>
2016-12-08 21:33:23 +0700

    creating initial actionTypes

---
+++ src/constants/actionTypes.js
@@ -0,0 +1,10 @@
+export const CREATE_USER = 'CREATE_USER';
+export const CREATE_ROLE = 'CREATE_ROLE';
+
+export const ADD_COMMENT = 'ADD_COMMENT';
+export const EDIT_COMMENT = 'EDIT_COMMENT';
+export const DELETE_COMMENT = 'DELETE_COMMENT';
+
+export const ADD_IDEA = 'ADD_IDEA';
+export const DELETE_IDEA = 'DELETE_IDEA';
+export const REPORT_IDEA = 'REPORT_IDEA';
