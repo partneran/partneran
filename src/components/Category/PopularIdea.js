564dddc8d897cb7f689fe2f69a9dcf251c457fcb
Septian A Tama <sfirst.tama@gmail.com>
2016-12-09 23:47:11 +0700

    modifiying popular idea

--- src/components/Category/PopularIdea.js
+++ src/components/Category/PopularIdea.js
@@ -2,12 +2,26 @@ import React from 'react';
 
 const PopularIdea = () => {
     return (
-        <div className="container">
-            <div className="section text-center">
-                <h2>Popular Ideas</h2>
-                <div className="row">
-                    <div className="col-md-4 text-left">
-                        <div className="idea_thumbnail hovereffect" style={{backgroundImage: 'url(https://avatars1.githubusercontent.com/u/10125796?v=3&u=b92ee420ae73b0fa745c0dbbbbf846630184fbb3&s=400)'}}>
+            <div className="container">
+              <div className="section text-center">
+                  <h2 className="poptitle text-info">Popular Ideas</h2>
+                  <div className="row">
+                    <div className="popular col-md-4 text-left">
+                      <div className="card">
+                        <div className="popular-thumb hovereffect" style={{backgroundImage: 'url(https://static.pexels.com/photos/7374/startup-photos.jpg)'}}>
+                            <div className="overlay">
+                                <div className="box">
+                                    <h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</h2>
+                              <p> <a href="#">LINK HERE</a> </p>
+                                </div>
+                            </div>
+                        </div>
+                      </div>
+                    </div>
+
+                    <div className="popular col-md-4 text-left">
+                      <div className="card">
+                        <div className="popular-thumb hovereffect" style={{backgroundImage: 'url(https://images.pexels.com/photos/7375/startup-photos.jpg?w=1260&h=750&auto=compress&cs=tinysrgb)'}}>
                             <div className="overlay">
                                 <div className="box">
                                     <h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</h2>
@@ -15,17 +29,45 @@ const PopularIdea = () => {
                                 </div>
                             </div>
                         </div>
-                        <div className="col-md-6 no-padding-left">
-                            <p className="small text-muted">
-                                <strong>Category: </strong>
-                                <span className="small-detail">Category</span>
-                            </p>
-                        </div>
+                      </div>
                     </div>
+                    <div className="popular col-md-4 text-left">
+                      <div className="card">
+                        <div className="popular-thumb hovereffect" style={{backgroundImage: 'url(https://static.pexels.com/photos/7110/desk-office-workspace-coworking.jpg)'}}>
+                            <div className="overlay">
+                                <div className="box">
+                                    <h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</h2>
+              				        <p> <a href="#">LINK HERE</a> </p>
+                                </div>
+                            </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+              </div>
+            </div>
+    )
+}
+
+const PopularIdeaItem = () => {
+  return (
+    <div className="popular col-md-4 text-left">
+      <div className="card">
+        <div className="popular-thumb hovereffect" style={{backgroundImage: 'url(https://static.pexels.com/photos/7374/startup-photos.jpg)'}}>
+            <div className="overlay">
+                <div className="box">
+                    <h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</h2>
+              <p> <a href="#">LINK HERE</a> </p>
                 </div>
             </div>
         </div>
-    )
+        <div className="col-md-6 no-padding-left">
+
+        </div>
+      </div>
+
+    </div>
+  )
 }
 
 export default PopularIdea
