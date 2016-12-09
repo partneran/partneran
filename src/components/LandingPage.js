7b120203b544702bbe81f8fbd7207073cf559b87
broerjuang <juangwiantoro@gmail.com>
2016-12-09 11:27:49 +0700

    modify root

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Intro from './Intro';
+import HelloHome from './HelloHome';
 
 const LandingPage = () => {
     return (
         <div className="wrapper">
             <Intro />
+            <HelloHome />
         </div>
     )
 }
--- src/routes.js
+++ src/routes.js
@@ -3,18 +3,17 @@ import { Route, IndexRoute } from 'react-router';
 import HelloHome from './components/HelloHome';
 import App from './App';
 import LandingPage from './components/LandingPage';
+// import SignupPage from './components/SignupPage';
+// import LoginPage from './components/LoginPage';
+// import ExplorePage from './components/ExplorePage';
+// import ProfilePage from'./components/ProfilePage';
+// import IdeaPage from './components/IdeaPage';
+
 
 export default (
      <Route path="/" component={App}>
         <IndexRoute component={LandingPage} />
-        <Route path="home" component={HelloHome} />
-        <Route path="home" component={HelloHome} />
-        <Route path="home" component={HelloHome} />
     </Route>
-//   <Route path="/" component={App}>
-  
-//     </Route>
-//     <Route path="/" component={LandingPage} />
 )
 
-    // <Route path="signup" component={SignupPage} />
+// <Route path="signup" component={Signup} />
