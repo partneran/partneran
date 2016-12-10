bcbfbe417d115f271a85c9ffa836d09bf9637af4
Septian A Tama <sfirst.tama@gmail.com>
2016-12-10 10:51:51 +0700

    add motivational sentences

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -6,8 +6,8 @@ const Intro = () => {
           <div className="container">
             <div className="row">
               <div className="col-md-6">
-                <h1 className="title">Write the best title for your page.</h1>
-                          <h4>Every landing page needs a small description after the big bold title, that's why we added this text here. Add here all the information that can make you or your product create the first impression.</h4>
+                <h1 className="title">Your idea is a start of something great . . .</h1>
+                          <h4>Each of us has a strength to make a difference and change the world. Don't let your idea for yourself, share it, find your partner, and collaborate with them to give a great impact to the society!</h4>
                           <br />
                           <button type="button" data-toggle="modal" data-target="#myModal" className="btn btn-info btn-raised btn-lg">
                   <i className="fa fa-play"></i> Watch video
