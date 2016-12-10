8874ceb1a393049c76f94ab7e8513ab87193a140
broerjuang <juangwiantoro@gmail.com>
2016-12-10 21:51:44 +0700

    creating input validation

--- src/actions/user.js
+++ src/actions/user.js
@@ -1,16 +1,25 @@
-import { SIGN_UP, LOG_IN, FORGET_PASSWORD, NEW_PASSWORD } from '../constants/actionTypes'
+import { SIGN_UP, 
+        LOG_IN, 
+        FORGET_PASSWORD, 
+        NEW_PASSWORD } from '../constants/actionTypes'
+
 import axios from 'axios';
+import { browserHistory } from 'react-router';
+import Auth from '../helpers/token';
 
 const uri = 'https://partneran-484c5.firebaseio.com/user'
 
-export const signup = (user) => 
+export const signup = user => 
     ({
         type: SIGN_UP,
         user:  axios
                 .post(uri, {
                     user
                 })
-                .then(res => console.log(res))
+                .then(res => {
+                    Auth.authenticateUser(res)
+                    browserHistory.push('/explore')
+                })
                 .catch(err => console.log(err))
     })
 
