c2c62864786376cca6cc21c1865240e24b785c96
broerjuang <juangwiantoro@gmail.com>
2016-12-11 22:26:57 +0700

    creating state in createIdea

--- src/components/Idea/CreateIdea.js
+++ src/components/Idea/CreateIdea.js
@@ -3,75 +3,178 @@ import Footer from '../Footer/Footer'
 import { Editor } from 'react-draft-wysiwyg';
 import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
 
-const CreateIdea = () => {
-  return (
-    <div className="components-page">
-      <div className="wrapper">
-        <div id="new-idea-intro" className="header header-filter">
-          <div className="container">
-            <div className="row text-center">
-              <h1 className="title">Share New Idea</h1>
-              <h5>Make a great impact! Start from sharing your idea!</h5>
-            </div>
-          </div>
-        </div>
-        <div className="main">
-          <div className="container">
-            <div className="row">
-              <div className="card form-card">
-                <div className="card-header" data-background-color="blue">
-                  <h4 className="title">Your Idea</h4>
-		              <p className="category">Tell us about your idea</p>
+class CreateIdea extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      title: "",
+      video: "",
+      image: "",
+      desctiption: ""
+    }
+    this.onChange = this.onChange.bind(this)
+    this.onSubmit = this.onSubmit.bind(this)
+  }
+
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  onSubmit(e) {
+    e.preventDefault()
+    console.log(this.state)
+  }
+  
+  render() {
+    const { title } = this.state
+    return (
+        <div className="components-page">
+          <div className="wrapper">
+            <div id="new-idea-intro" className="header header-filter">
+              <div className="container">
+                <div className="row text-center">
+                  <h1 className="title">Share New Idea</h1>
+                  <h5>Make a great impact! Start from sharing your idea!</h5>
                 </div>
-                <div className="card-signup">
-                  <div className="container">
-                    <form encType="multipart/form-data">
-                      <div className="form-group label-floating">
-                        <label className="control-label">Your Idea's Title</label>
-                        <input type="text" className="form-control" required />
+              </div>
+            </div>
+            <div className="main">
+              <div className="container">
+                <div className="row">
+                  <div className="card form-card">
+                    <div className="card-header" data-background-color="blue">
+                      <h4 className="title">Your Idea</h4>
+                      <p className="category">Tell us about your idea</p>
+                    </div>
+                    <div className="card-signup">
+                      <div className="container">
+                        <form encType="multipart/form-data" onSubmit={this.onSubmit}>
+                          <div className="form-group label-floating">
+                            <label className="control-label">Your Idea's Title</label>
+                            <input 
+                              type="text" 
+                              className="form-control" 
+                              name="title"
+                              value={title}
+                              onChange={this.onChange}
+                              required />
+                          </div>
+                          <p>{title}</p>
+                          <div className="form-group label-floating">
+                            <label className="control-label">Featured Video (Youtube-Link)</label>
+                            <input type="text" className="form-control" required />
+                          </div>
+                          <div className="form-group label-floating">
+                            <div className="col-md-10 no-padding-left">
+                            <input type="text" className="form-control" placeholder="Featured Image URL"/>
+                            </div>
+                            <div className="col-md-2 no-padding-left">
+                            <label className="btn btn-info btn-sm">Upload Image</label>
+                            <input type="file" id="exampleInputFile" required/>
+                            </div>
+                          </div>
+                          <br/>
+                          <br/>
+                          <div className="form-group label-floating">
+                            <label className="control-label">Category</label>
+                            <select className="form-control">
+                              <option>Lalala</option>
+                              <option>Lalala</option>
+                              <option>Lalala</option>
+                            </select>
+                          </div>
+                          <div className="form-group">
+                            <br/>
+                            <label>Your Idea Detail</label>
+                            <Editor/>
+                          </div>
+                          <div className="form-group">
+                            <button type="submit" className="btn btn-info">Submit</button>
+                          </div>
+                        </form>
                       </div>
-                      <div className="form-group label-floating">
-                        <label className="control-label">Featured Video (Youtube-Link)</label>
-                        <input type="text" className="form-control" required />
-                      </div>
-                      <div className="form-group label-floating">
-                        <div className="col-md-10 no-padding-left">
-                        <input type="text" className="form-control" placeholder="Featured Image URL"/>
-                        </div>
-                        <div className="col-md-2 no-padding-left">
-                        <label className="btn btn-info btn-sm">Upload Image</label>
-                        <input type="file" id="exampleInputFile" required/>
-                        </div>
-                      </div>
-                      <br/>
-                      <br/>
-                      <div className="form-group label-floating">
-                        <label className="control-label">Category</label>
-                        <select className="form-control">
-                          <option>Lalala</option>
-                          <option>Lalala</option>
-                          <option>Lalala</option>
-                        </select>
-                      </div>
-                      <div className="form-group">
-                        <br/>
-                        <label>Your Idea Detail</label>
-                        <Editor/>
-                      </div>
-                      <div className="form-group">
-                        <button type="button" className="btn btn-info">Submit</button>
-                      </div>
-                    </form>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
+          <Footer />
         </div>
-      </div>
-      <Footer />
-    </div>
-  )
+    )
+  }
 }
+
+
+// const CreateIdea = () => {
+//   return (
+//     <div className="components-page">
+//       <div className="wrapper">
+//         <div id="new-idea-intro" className="header header-filter">
+//           <div className="container">
+//             <div className="row text-center">
+//               <h1 className="title">Share New Idea</h1>
+//               <h5>Make a great impact! Start from sharing your idea!</h5>
+//             </div>
+//           </div>
+//         </div>
+//         <div className="main">
+//           <div className="container">
+//             <div className="row">
+//               <div className="card form-card">
+//                 <div className="card-header" data-backgrou`nd-color="blue">
+//                   <h4 className="title">Your Idea</h4>
+// 		              <p className="category">Tell us about your idea</p>
+//                 </div>
+//                 <div className="card-signup">
+//                   <div className="container">
+//                     <form encType="multipart/form-data">
+//                       <div className="form-group label-floating">
+//                         <label className="control-label">Your Idea's Title</label>
+//                         <input type="text" className="form-control" required />
+//                       </div>
+//                       <div className="form-group label-floating">
+//                         <label className="control-label">Featured Video (Youtube-Link)</label>
+//                         <input type="text" className="form-control" required />
+//                       </div>
+//                       <div className="form-group label-floating">
+//                         <div className="col-md-10 no-padding-left">
+//                         <input type="text" className="form-control" placeholder="Featured Image URL"/>
+//                         </div>
+//                         <div className="col-md-2 no-padding-left">
+//                         <label className="btn btn-info btn-sm">Upload Image</label>
+//                         <input type="file" id="exampleInputFile" required/>
+//                         </div>
+//                       </div>
+//                       <br/>
+//                       <br/>
+//                       <div className="form-group label-floating">
+//                         <label className="control-label">Category</label>
+//                         <select className="form-control">
+//                           <option>Lalala</option>
+//                           <option>Lalala</option>
+//                           <option>Lalala</option>
+//                         </select>
+//                       </div>
+//                       <div className="form-group">
+//                         <br/>
+//                         <label>Your Idea Detail</label>
+//                         <Editor/>
+//                       </div>
+//                       <div className="form-group">
+//                         <button type="button" className="btn btn-info">Submit</button>
+//                       </div>
+//                     </form>
+//                   </div>
+//                 </div>
+//               </div>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//       <Footer />
+//     </div>
+//   )
+// }
 
 export default CreateIdea;
