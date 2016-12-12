c954d0023461d345a7d460ba861b8f3f304e7433
Septian A Tama <sfirst.tama@gmail.com>
2016-12-12 13:58:13 +0700

    add idea to mainpage

---
+++ src/components/Category/AllIdea.js
@@ -0,0 +1,80 @@
+import React from 'react';
+
+const AllIdea = () => {
+    return (
+        <div>
+                <h2 className="explore-title">All Ideas</h2>
+                <div className="row">
+                    <div className="col-md-4 text-left">
+                      <div className="card thumbnail">
+
+                        <img src="https://images.pexels.com/photos/205316/pexels-photo-205316.png?dl&fit=crop&w=640&h=426" alt="..."  className="img-responsive"/>
+                        <div className="caption">
+                          <h3>Idea Title</h3>
+                          <p>
+                            <i className="fa fa-icon fa-user"></i> Ken Duigraha
+                            &nbsp;
+                            <i className="fa fa-icon fa-tag"></i> Social Network
+                            &nbsp;
+                            <i className="fa fa-icon fa-calendar"></i> Dec 12th, 2016
+                          </p>
+                          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                          <ul className="pager">
+                            <li className="next"><a href="#">See Details <span aria-hidden="true">&rarr;</span></a></li>
+                          </ul>
+                        </div>
+
+                      </div>
+                    </div>
+                    <div className="col-md-4 text-left">
+                      <div className="card thumbnail">
+
+                        <img src="https://images.pexels.com/photos/39284/macbook-apple-imac-computer-39284.jpeg?dl&fit=crop&w=640&h=425" alt="..."  className="img-responsive"/>
+                        <div className="caption">
+                          <h3>Idea Title</h3>
+                          <p>
+                            <i className="fa fa-icon fa-user"></i> Ken Duigraha
+                            &nbsp;
+                            <i className="fa fa-icon fa-tag"></i> Social Network
+                            &nbsp;
+                            <i className="fa fa-icon fa-calendar"></i> Dec 12th, 2016
+                          </p>
+                          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                          <ul className="pager">
+                            <li className="next"><a href="#">See Details <span aria-hidden="true">&rarr;</span></a></li>
+                          </ul>
+                        </div>
+
+                      </div>
+                    </div>
+
+                    <div className="col-md-4 text-left">
+                      <div className="card thumbnail">
+
+                        <img src="https://images.pexels.com/photos/39284/macbook-apple-imac-computer-39284.jpeg?dl&fit=crop&w=640&h=425" alt="..."  className="img-responsive"/>
+                        <div className="caption">
+                          <h3>Idea Title</h3>
+                          <p>
+                            <i className="fa fa-icon fa-user"></i> Ken Duigraha
+                            &nbsp;
+                            <i className="fa fa-icon fa-tag"></i> Social Network
+                            &nbsp;
+                            <i className="fa fa-icon fa-calendar"></i> Dec 12th, 2016
+                          </p>
+                          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                          <ul className="pager">
+                            <li className="next"><a href="#">See Details <span aria-hidden="true">&rarr;</span></a></li>
+                          </ul>
+                        </div>
+
+                      </div>
+                    </div>
+                </div>
+                <ul className="pager">
+                  <li className="next"><a href="#">Explore more <span aria-hidden="true">&rarr;</span></a></li>
+                </ul>
+        </div>
+    )
+}
+
+export default AllIdea
