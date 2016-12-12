09d00b02bae7475869f8161be804f2f6125022c8
broerjuang <juangwiantoro@gmail.com>
2016-12-12 16:36:30 +0700

    Latest Idea card

--- src/components/Category/LatestIdea.js
+++ src/components/Category/LatestIdea.js
@@ -1,83 +1,44 @@
-import React from 'react';
+import React from 'react'
+import { connect } from 'react-redux'
+import { Link } from 'react-router'
+import { sampleSize } from 'lodash'
+import IdeaCard from './IdeaCard'
 
-const LatestIdea = () => {
+
+const Latest = ({ ideas }) => {
+    // sampleSize from lodash takes two arguments in which the first is the array, and the second is the picked element randomly
+      const sampleIdea = sampleSize(ideas, 3)
+      const latestIdeaList = sampleIdea.map(i => <IdeaCard image={i.image} 
+                                                              key={i.id} 
+                                                              title={i.title} 
+                                                              author={i.author} 
+                                                              social={i.social}
+                                                              join={i.join}
+                                                              description={i.description}                                            
+                                              />)
     return (
-        <div className="container">
-            <div className="section text-center">
-                <h2>Newest Ideas</h2>
-                <div className="row">
-                    <div className="col-md-4 text-left">
-                      <div className="card thumbnail">
-
-                        <img src="https://images.pexels.com/photos/205316/pexels-photo-205316.png?dl&fit=crop&w=640&h=426" alt="..."  className="img-responsive"/>
-                        <div className="caption">
-                          <h3>Idea Title</h3>
-                          <p>
-                            <i className="fa fa-icon fa-user"></i> Ken Duigraha
-                            &nbsp;
-                            <i className="fa fa-icon fa-tag"></i> Social Network
-                            &nbsp;
-                            <i className="fa fa-icon fa-calendar"></i> Dec 12th, 2016
-                          </p>
-                          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                          <ul className="pager">
-                            <li className="next"><a href="#">See Details <span aria-hidden="true">&rarr;</span></a></li>
-                          </ul>
-                        </div>
-
-                      </div>
-                    </div>
-
-                    <div className="col-md-4 text-left">
-                      <div className="card thumbnail">
-
-                        <img src="https://images.pexels.com/photos/39284/macbook-apple-imac-computer-39284.jpeg?dl&fit=crop&w=640&h=425" alt="..."  className="img-responsive"/>
-                        <div className="caption">
-                          <h3>Idea Title</h3>
-                          <p>
-                            <i className="fa fa-icon fa-user"></i> Ken Duigraha
-                            &nbsp;
-                            <i className="fa fa-icon fa-tag"></i> Social Network
-                            &nbsp;
-                            <i className="fa fa-icon fa-calendar"></i> Dec 12th, 2016
-                          </p>
-                          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                          <ul className="pager">
-                            <li className="next"><a href="#">See Details <span aria-hidden="true">&rarr;</span></a></li>
-                          </ul>
-                        </div>
-
-                      </div>
-                    </div>
-
-                    <div className="col-md-4 text-left">
-                      <div className="card thumbnail">
-
-                        <img src="https://images.pexels.com/photos/38544/imac-apple-mockup-app-38544.jpeg?dl&fit=crop&w=640&h=425" alt="..."  className="img-responsive"/>
-                        <div className="caption">
-                          <h3>Idea Title</h3>
-                          <p>
-                            <i className="fa fa-icon fa-user"></i> Ken Duigraha
-                            &nbsp;
-                            <i className="fa fa-icon fa-tag"></i> Social Network
-                            &nbsp;
-                            <i className="fa fa-icon fa-calendar"></i> Dec 12th, 2016
-                          </p>
-                          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                          <ul className="pager">
-                            <li className="next"><a href="#">See Details <span aria-hidden="true">&rarr;</span></a></li>
-                          </ul>
-                        </div>
-
-                      </div>
-                    </div>
-                </div>
-                <ul className="pager">
-                  <li className="next"><a href="#">Explore More <span aria-hidden="true">&rarr;</span></a></li>
-                </ul>
+            <div className="container">
+              <div className="section text-center">
+                  <h2 className="poptitle text-info">Newest Idea</h2>
+                  <div className="row">
+                    {latestIdeaList}
+                  </div>
+                  <ul className="pager">
+                    <li className="next">
+                      <Link to="explore"> Explore More  <span aria-hidden="true">&rarr;</span></Link>
+                      </li>
+                  </ul>
+              </div>
             </div>
-        </div>
     )
 }
 
-export default LatestIdea
+
+const mapStateToProps = (state) => {
+  return {
+    ideas: state.ideas
+  }
+}
+
+
+export default connect(mapStateToProps, null)(Latest)
