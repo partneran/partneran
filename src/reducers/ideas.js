9b6a7465e15eca66d16bcb55c3484152febde86d
broerjuang <juangwiantoro@gmail.com>
2016-12-12 16:02:52 +0700

    adding IdeaCard component

---
+++ src/reducers/ideas.js
@@ -0,0 +1,71 @@
+
+import v4 from 'uuid/v4';
+
+import {
+    ADD_IDEA
+} from '../constants/actionTypes'
+
+const initialState = [
+    {
+        title: 'Title 1',
+        image: 'https://static.pexels.com/photos/7374/startup-photos.jpg',
+        description: 'This is description from the first title',
+        slug: 'http:localhost:3000/firstidea',
+        id: '1',
+        author: 'Ken',
+        social: 'http://twitter.com',
+        join: '12 December 2016'
+    },
+    {
+        title: 'Title 2',
+        image: 'https://static.pexels.com/photos/7374/startup-photos.jpg',
+        description: 'This is description from the Second title',
+        slug: 'http:localhost:3000/firstidea',
+        id: '2',
+        author: 'tama',
+        social: 'http://twitter.com',
+        join: '12 December 2016'
+    },
+    {
+        title: 'Title 3',
+        image: 'https://static.pexels.com/photos/7374/startup-photos.jpg',
+        description: 'This is description from the third title',
+        slug: 'http:localhost:3000/firstidea',
+        id: '3',
+        author: 'juang',
+        social: 'http://twitter.com',
+        join: '12 December 2016'
+    }
+]
+
+
+
+
+const idea = (state = {}, action) => {
+    switch(action.type){
+        case ADD_IDEA:
+            return {
+                idea_id: v4(),
+                title: action.payload.title,
+                picture: action.payload.picture,
+                video: action.payload.video,
+                category: action.payload.category
+            }
+        default:
+            return state
+    }
+}
+
+const ideas = (state = initialState, action) => {
+    switch(action.type){
+        case ADD_IDEA:
+            return [
+                ...state,
+                idea(undefined, action)
+            ]
+        default:
+            return state;
+    }
+}
+
+export default ideas
