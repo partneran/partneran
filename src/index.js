14c05f61b8202f52128ae17cec92d2bcbfa9349b
broerjuang <juangwiantoro@gmail.com>
2016-12-12 12:20:55 +0700

    separating store from main index

--- src/index.js
+++ src/index.js
@@ -3,29 +3,31 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, browserHistory } from 'react-router';
 import { Provider} from 'react-redux';
-
-// Redux dependency
-import { createStore, applyMiddleware } from 'redux';
-import reducers from './reducers';
-import thunk from 'redux-thunk';
-import promise from 'redux-promise';
-import logger from 'redux-logger';
-
-// axios for calling server
-import axios from 'axios';
-import axiosMiddleware from 'redux-axios-middleware';
-
-// internal dipendency
 import routes from './routes';
+import store from './store'
 
-const client = axios.create({ 
-  baseURL:'https://partneran-484c5.firebaseio.com/',
-  responseType: 'json'
-});
+// // Redux dependency
+// import { createStore, applyMiddleware } from 'redux';
+// import reducers from './reducers';
+// import thunk from 'redux-thunk';
+// import promise from 'redux-promise';
+// import logger from 'redux-logger';
 
-const middleware = applyMiddleware(thunk, promise, logger(), axiosMiddleware(client))
+// // axios for calling server
+// import axios from 'axios';
+// import axiosMiddleware from 'redux-axios-middleware';
 
-const store = createStore(reducers, middleware)
+// // internal dipendency
+// import routes from './routes';
+
+// const client = axios.create({ 
+//   baseURL:'https://partneran-484c5.firebaseio.com/',
+//   responseType: 'json'
+// });
+
+// const middleware = applyMiddleware(thunk, promise, logger(), axiosMiddleware(client))
+
+// const store = createStore(reducers, middleware)
 
 
 
---
+++ src/store.js
@@ -0,0 +1,10 @@
+import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import logger from 'redux-logger'
+import promise from 'redux-promise'
+import reducer from './reducers'
+
+
+const middleware = applyMiddleware(thunk, promise, logger())
+
+export default createStore(reducer, middleware)
