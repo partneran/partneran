486c3876b94878b9b063cc5d18f81a7d96bd9ffb
broerjuang <juangwiantoro@gmail.com>
2016-12-13 14:49:39 +0700

    protecting signup and login from login user

--- src/components/Login/LoginPage.js
+++ src/components/Login/LoginPage.js
@@ -1,25 +1,32 @@
-import React from 'react';
-import LoginForm from './LoginForm';
-import Footer from '../Footer/Footer';
+import React, { Component } from 'react'
+import LoginForm from './LoginForm'
+import Footer from '../Footer/Footer'
+import Auth from '../../helpers/token'
 
 
-const LoginPage = () => {
-  return (
-    <div className="signup-page">
-     <div className="wrapper">
-        <div className="header header-filter" style={{backgroundImage: 'url(../assets/img/city.jpg)', backgroundSize: 'cover', backgroundPosition: 'top center'}}>
-          <div className="container">
-            <div className="row">
-              <div className="col-md-4 col-md-offset-4 col-sm-6 col-sm-offset-3">
-                <LoginForm />
+class LoginPage extends Component {
+  componentDidMount(){
+    !Auth.getToken() ? this.props.router.replace('/login') : this.props.router.replace('/explore')
+  }
+
+  render() {
+    return (
+      <div className="signup-page">
+      <div className="wrapper">
+          <div className="header header-filter" style={{backgroundImage: 'url(../assets/img/city.jpg)', backgroundSize: 'cover', backgroundPosition: 'top center'}}>
+            <div className="container">
+              <div className="row">
+                <div className="col-md-4 col-md-offset-4 col-sm-6 col-sm-offset-3">
+                  <LoginForm />
+                </div>
               </div>
             </div>
           </div>
         </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
-  )
+    )
+  }
 }
 
 export default LoginPage
--- src/components/Signup/SignupPage.js
+++ src/components/Signup/SignupPage.js
@@ -1,25 +1,33 @@
-import React from 'react';
-import SignupForm from './SignupForm';
-import Footer from '../Footer/Footer';
+import React, { Component } from 'react'
+import SignupForm from './SignupForm'
+import Footer from '../Footer/Footer'
+import Auth from '../../helpers/token'
 
 
-const SignupPage = () => {
-  return (
-    <div className="signup-page">
-     <div className="wrapper">
-        <div className="header header-filter" style={{backgroundImage: 'url(../assets/img/city.jpg)', backgroundSize: 'cover', backgroundPosition: 'top center'}}>
-          <div className="container">
-            <div className="row">
-              <div className="col-md-4 col-md-offset-4 col-sm-6 col-sm-offset-3">
-                <SignupForm />
+class SignupPage extends Component {
+
+  componentDidMount(){
+    !Auth.getToken() ? this.props.router.replace('/login') : this.props.router.replace('/explore')
+  }
+
+  render() {
+    return (
+      <div className="signup-page">
+      <div className="wrapper">
+          <div className="header header-filter" style={{backgroundImage: 'url(../assets/img/city.jpg)', backgroundSize: 'cover', backgroundPosition: 'top center'}}>
+            <div className="container">
+              <div className="row">
+                <div className="col-md-4 col-md-offset-4 col-sm-6 col-sm-offset-3">
+                  <SignupForm />
+                </div>
               </div>
             </div>
           </div>
         </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
-  ) 
+    ) 
+  }
 }
 
 export default SignupPage
