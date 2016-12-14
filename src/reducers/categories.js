50ba7b04fd7a97bdefc833b232b860b8019b7cff
broerjuang <juangwiantoro@gmail.com>
2016-12-14 10:26:17 +0700

    matching the category name with the server

--- src/reducers/categories.js
+++ src/reducers/categories.js
@@ -1,4 +1,4 @@
-const initialState = ['Please choose one', 'E-Commerce', 'Education Technology', 'Financial Technology', 'Internet of Thing', 'Art and Culture', 'Agriculture', 'Health and Lifestyle', 'On Demand Service', 'Games', 'Software', 'Mobile']
+const initialState = ['Please choose one', 'E-Commerce', 'EdTech', 'FinTech', 'IoT', 'Art & Culture', 'Agriculture', 'Health & Lifestyle', 'On Demand Service', 'Games', 'Software', 'Mobile']
 
 const categories = (state = initialState, action) => {
     switch(action.type){
